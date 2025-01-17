c80e8b63c11a4eb8b34595fa45d4ce4361708f2c
Kevin ZITNIK <zitnik.kevin@gmail.com>
2025-01-17 15:46:52 +0100

    feat: add flash card controller

---
+++ src/Controller/FlashCardController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\FlashCardType;
+use App\HttpClient\ApiHttpClient;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+final class FlashCardController extends AbstractController{
+    #[Route('/flashcard/card', name: 'app_flashcard')]
+    public function index(): Response
+    {
+        return $this->render('flash_card/index.html.twig', [
+            'controller_name' => 'FlashCardController',
+        ]);
+    }
+
+    #[Route('/flashcard/new', name: 'app_home_flashcard_form')]
+    public function flashCardForm(Request $request, ApiHttpClient $apiHttpClient): Response
+    {
+        $user = $this->getUser();
+
+        $flashCardForm = $this->createForm(FlashCardType::class);
+
+        $flashCardForm->handleRequest($request);
+        
+        if($flashCardForm->isSubmitted() && $flashCardForm->isValid()) {
+            $question = $flashCardForm->get('question')->getData();
+            $answer = $flashCardForm->get('answer')->getData();
+            $userId = $user->getId();
+
+            $response = $apiHttpClient->addFlashcard([
+                "question"=> $question,
+                "answer"=> $answer,
+                "userId"=> $userId
+            ]);
+
+            return $this->redirectToRoute('app_home', [
+                
+            ]);
+            
+        }
+
+        return $this->render('flashcard/flashcard_form.html.twig', [
+            'form' => $flashCardForm,
+        ]);
+    }
+}
